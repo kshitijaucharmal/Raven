4cd9a2c915f448911de5936ed000b06b372c314e
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-07-27 14:31:49 +0530

    Button redirect to login

--- frontend/src/pages/404ErrorPage.tsx
+++ frontend/src/pages/404ErrorPage.tsx
@@ -4,15 +4,29 @@ import {
     Flex,
     Text,
     Link as LinkButton,
+    Callout
 } from "@radix-ui/themes";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+
+
 
 const NotFoundPage = () => {
+    
+    const navigate = useNavigate();
+    const handleNavigation = () => {
+        navigate("/login");
+    }
 
     return (
-        <Flex direction="column" gap="2">
+        <Flex direction="column" gap="3" align={"center"}>
             <Text className="cal-sans" align="center" size="3"> Error Code 404 </Text>
             <Text className="cal-sans" align="center" size="8"> PAGE NOT FOUND </Text>
-            <Button color="orange" variant="surface">Go Back</Button>
+                <Button className="w-[100vw]"  variant="surface" color="orange" onClick={handleNavigation}> 
+                    <Link to='/login'> GO BACK</Link>
+                </Button>
         </Flex>
     )
 }
